6650659711c633ba465a00e4a87b7e84af14aaa9
ariellyrycs <ariel_556@hotmail.com>
2014-10-13 23:51:48 -0700

    mongodb implemented

--- api/routes/pets.js
+++ api/routes/pets.js
@@ -1,37 +1,107 @@
-/**
- * Created by arobles on 10/10/14.
- */
+'use strict';
+var Pet = require('../models/pet.js');
 
-module.exports = function (app, Datastore, db) {
-    'use strict';
-    //pets
-    db.pets = new Datastore({ filename: 'api/db/pets', autoload: true});
+module.exports = function(app) {
+    var findAllPets = function(req, res) {
+        return Pet.find(function(err, pets) {
+            if(!err) {
+                return res.send(pets);
+            } else {
+                res.statusCode = 500;
+                console.error('Error', res.statusCode, err.message);
+                return res.send({ error: 'Server error' });
+            }
+        });
+        },
+        findById = function(req, res) {
+            return Pet.findById(req.params.id, function(err, pet) {
+                if(!pet) {
+                    res.statusCode = 404;
+                    return res.send({ error: 'Not found' });
+                }
+                if(!err) {
+                    return res.send({ status: 'OK', pet:pet });
+                } else {
+                    res.statusCode = 500;
+                    console.error('Error', res.statusCode, err.message);
+                    return res.send({ error: 'Server error' });
+                }
+            });
+        },
+        addPet = function(req, res) {
+            console.log(req.body);
+            var pet = new Pet({
+                animal:    req.body.animal,
+                age :    req.body.age,
+                color:    req.body.color
+            });
+            pet.save(function(err) {
+                var toPrint = null;
+                if(err) {
+                    console.log('Error while saving pet: ' + err);
+                    res.send({ error:err });
+                } else {
+                    console.log("Pet created");
+                    toPrint =  res.send({ status: 'OK', pet:pet });
+                }
+                return toPrint;
+            });
+        },
+        updatePet = function(req, res) {
+            return Pet.findById(req.params.id, function(err, pet) {
+                if(!pet) {
+                    res.statusCode = 404;
+                    return res.send({ error: 'Not found' });
+                }
+                if (req.body.animal !== null) {
+                    pet.animal = req.body.animal;
+                }
+                if (req.body.age !== null) {
+                    pet.age = req.body.age;
+                }
+                if (req.body.color !== null) {
+                    pet.color = req.body.color;
+                }
+                return pet.save(function(err) {
+                    if(!err) {
+                        console.log('Updated');
+                        return res.send({ status: 'OK', pet:pet });
+                    } else {
+                        if(err.name === 'ValidationError') {
+                            res.statusCode = 400;
+                            res.send({ error: 'Validation error' });
+                        } else {
+                            res.statusCode = 500;
+                            res.send({ error: 'Server error' });
+                        }
+                        console.error('Error',res.statusCode,err.message);
+                    }
+                    return res.send(pet);
 
-    app.get('/owners/pets', function (req, res) {
-        db.pets.find({}, res.locals.respond);
-    });
-
-    app.post('/owners/pets', function (req, res) {
-        console.log(req.body);
-        db.pets.insert({ name: req.body.name, owner_id: req.body.owner }, res.locals.respond);
-    });
-
-    /*
-     app.get('/owners/:ownerId/pets/:petId', function (req, res) {
-        db.pets.findOne({ pet_id: req.params.petId, owner_id: req.params.ownerId}, res.locals.respond);
-     });
-
-     app.put('/owners/:ownerId/pets/:petId' , function (req, res) {
-         db.pets.update({ pet_id: req.params.petId, owner_id: req.params.ownerId }, req.body, function (err, num) {
-            res.locals.respond(err, { success: num + " updated" });
-         })
-     });
-
-     app.delete('/owners/:ownerId/pets/:petId', function (req, res) {
-         db.pets.remove({ pet_id: req.params.petId, owner_id: req.params.ownerId }, function (err, num) {
-            res.locals.respond(err, { success: num + " deleted"});
-         });
-     });
-
-     */
+                });
+            });
+        },
+        deletePet = function(req, res) {
+            return Pet.findById(req.params.id, function(err, pet) {
+                if(!pet) {
+                    res.statusCode = 404;
+                    return res.send({ error: 'Not found' });
+                }
+                return pet.remove(function(err) {
+                    if(!err) {
+                        console.log('Removed pet');
+                        return res.send({ status: 'OK' });
+                    } else {
+                        res.statusCode = 500;
+                        console.error('Error', res.statusCode,err.message);
+                        return res.send({ error: 'Server error' });
+                    }
+                });
+            });
+    };
+    app.get('/pets', findAllPets);
+    app.get('/pet/:id', findById);
+    app.post('/pet', addPet);
+    app.put('/pet/:id', updatePet);
+    app.delete('/pet/:id', deletePet);
 };
--- api/server.js
+++ api/server.js
@@ -1,22 +1,15 @@
 'use strict';
 
 var express = require('express'),
-    //mongoose = require('mongoose'),
+    mongoose = require('mongoose'),
     responder = require('./httpResponder'),
     bodyParser  = require('body-parser'),
-    fs = require('fs'),
-    Datastore = require('nedb'),
-
-    app = express(),
-    db = {},
-    routeFile;
+    app = express();
 
 
 app.set('port', process.env.PORT || 3000);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(function (req, res, next){
     res.type('application/json');
     res.locals.respond = responder.setup(res);
@@ -24,14 +17,15 @@ app.use(function (req, res, next){
 });
 
 
-var jsFiles = new RegExp(".(js)$", "i");
-fs.readdirSync(__dirname + '/routes').forEach(function (fileName) {
-    if(jsFiles.test(fileName)) {
-        routeFile = require(__dirname + '/routes/' + fileName);
-        routeFile(app, Datastore, db);
+require('./routes/pets.js')(app);
+// MongoDB configuration
+mongoose.connect('mongodb://localhost/pets', function(err) {
+    if(err) {
+        console.log('error connecting to MongoDB Database. ' + err);
+    } else {
+        console.log('Connected to Database');
     }
 });
-
 app.listen(app.get('port'), function () {
     console.log('listening on port 3000');
 });
